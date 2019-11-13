692fc355aa6c9940dbce91aeaad8eedc5719bd0d
Jeffrey Martinez <martinezjf2@gmail.com>
2019-11-13 03:45:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,11 +1,12 @@
 first_number = "12"
-
 second_number = "8"
 
-sum = first_number + second_number
 
-difference = first_number - second_number
+sum = "first_number" + "second_number"
 
-product = first_number * second_number
+difference = "first_number" - "second_number"
+
+product = "first_number" * "second_number"
+
 
 
